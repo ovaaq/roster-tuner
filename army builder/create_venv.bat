4aa4500a2887aff200df45910729b7b377433b21
Paavo Koivistoinen <paavo.koivistoinen@gmail.com>
2024-10-28 18:56:33 +0200

    Installation & data modifications
    Venv to py 3.12 and updating jsons to help update costs directly in one file

--- army builder/create_venv.bat
+++ army builder/create_venv.bat
@@ -3,16 +3,17 @@ call virtualenv venv
 
 call venv\Scripts\activate
 
-call pip install pandas
-call pip install PyInquirer
-call pip install pprintpp
-call pip install urllib3
-call pip install InquirerPy
-call pip install pywin32
-call pip install xlsxwriter
-call pip install jinja2
-call pip install simple_namespace
-call pip install rich
-pip install md2pdf
+call py -m pip install pandas
+call py -m pip install PyInquirer
+call py -m pip install pprintpp
+call py -m pip install urllib3
+call py -m pip install InquirerPy
+call py -m pip install pywin32
+call py -m pip install xlsxwriter
+call py -m pip install jinja2
+call py -m pip install simple_namespace
+call py -m pip install rich
+call py -m pip install md2pdf
+call py -m pip install weasyprint
 
 pause
